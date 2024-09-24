51a9cc327bd4d09ddc3b4da341b3833269b1d7ea
cristianbrunone <cristianbrunonecmbc@gmail.com>
2024-09-24 16:52:14 -0300

    commit

--- objeto/destructuring.js
+++ objeto/destructuring.js
@@ -4,3 +4,7 @@ let user = { name: "Cristian", edad: 26 };
 let { name: username, apellido = "brunone" } = user; //lado derecho las variables en que se van a separar las propiedades del objeto
 
 console.log(apellido);
+
+let { name, ...sobrantes } = user;
+
+console.log(sobrantes);
